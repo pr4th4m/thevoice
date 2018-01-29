45a478055cd8c0ac353a88e2a9ef93a67199d3b3
Prathamesh Nevagi <prathamesh.nevagi@M255780YJF6T6.community.veritas.com>
2018-01-29 15:53:48 +0530

    OM

---
+++ thevoice/urls.py
@@ -0,0 +1,37 @@
+"""thevoice URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from django.conf.urls import url, include
+from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token
+from thevoice.team import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^login/', obtain_jwt_token),
+
+    # NOTE: The below urls are optional
+    # They are used by django rest framework browsable api
+    # and django admin panel
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('admin/', admin.site.urls),
+]
