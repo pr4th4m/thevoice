ffd7999d916fd739a07a906f942f7b2615a5f955
pratz <pratz0909@gmail.com>
2018-02-02 17:23:23 +0530

    Model and fixtures completed for performance app

--- performance/models.py
+++ performance/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Performance(models.Model):
+    date = models.DateTimeField()
+    song = models.CharField(max_length=140)
+    candidate = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0} on {1}".format(self.song, self.date)
--- thevoice/settings.py
+++ thevoice/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'team.apps.TeamConfig',
+    'performance.apps.PerformanceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
